0c5d90de80ba0894037fc494023b13282c54ceac
Aidan Draper <adraper2@elon.edu>
2018-06-05 13:25:33 -0400

    removed remote_sense.R file due to lack of use

--- remote_sensing.R
+++ remote_sensing.R
@@ -13,10 +13,13 @@ library(ggmap)
 
 setwd("~/Documents/Junior_Year/DISC_REU/DISC_chesapeake/Landsat8/LC08_L1TP_015033_20160718_20170222_01_T1")
 
+band1 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B1.TIF")
 band2 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B2.TIF")
 band3 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B3.TIF")
 band4 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B4.TIF")
 band5 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B5.TIF")
+band6 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B2.TIF")
+band7 <- raster("LC08_L1TP_015033_20160718_20170222_01_T1_B2.TIF")
 
 # import dominant species dataset
 species <- read.csv("~/Documents/Junior_Year/DISC_REU/DISC_chesapeake/DominantSpPerPlot.csv")
@@ -25,18 +28,20 @@ species <- species[!is.na(species$easting),]
 species <- species[!is.na(species$northing),]
 
 # convert UTM to lat, long
-utm.coor <- SpatialPoints(cbind(species$easting,species$northing), 
+utm.coor.serc <- SpatialPoints(cbind(species$easting,species$northing), 
                           proj4string=CRS("+proj=utm +zone=18"))
 
 # Convert to lat/long
-long.lat.coor <- as.data.frame(spTransform(utm.coor,CRS("+proj=longlat")))
+long.lat.coor.serc <- as.data.frame(spTransform(utm.coor.serc,CRS("+proj=longlat")))
 
 species.map <- data.frame(species = species$simplifiedClass,
-                          lat = long.lat.coor$coords.x1,
-                          lon = long.lat.coor$coords.x2)
+                          lat = long.lat.coor.serc$coords.x1,
+                          lon = long.lat.coor.serc$coords.x2,
+                          easting = utm.coor.serc$coords.x1,
+                          northing = utm.coor.serc$coords.x2)
 
 # basic color plot
-rgb.stack <- stack(band2, band3, band4)
+rgb.stack <- stack(band4, band3, band2)
 plotRGB(rgb.stack, stretch ="hist")
 
 # reflection of vegetation (using NDVI)
@@ -54,11 +59,11 @@ hist(ndvi)
 
 # first, I need to convert the NDVI points to latitude longitude
 ndvi.p <- rasterToPoints(ndvi)
-utm.coor <- SpatialPoints(cbind(ndvi.points[,1],ndvi.points[,2]), 
+utm.coor <- SpatialPoints(cbind(ndvi.p[,1],ndvi.p[,2]), 
                           proj4string=CRS("+proj=utm +zone=18"))
 long.lat.coor <- as.data.frame(spTransform(utm.coor,CRS("+proj=longlat")))
 
-ndvi.points <- data.frame(layer=ndvi.p[,3], lat=long.lat.coor$coords.x1, lon=long.lat.coor$coords.x2)
+ndvi.points <- data.frame(layer=ndvi.p[,3], lat=long.lat.coor$coords.x1, lon=long.lat.coor$coords.x2, easting = utm.coor$coords.x1, northing = utm.coor$coords.x2)
 
 # constrain the ndvi points to the plot region
 ndvi.points <- ndvi.points[which(ndvi.points$lat > -76.555 & ndvi.points$lat < -76.537),]
@@ -75,7 +80,7 @@ ggmap(plot.map) +
   theme(legend.position="none")
 
 
-sample.ndvi.points <- ndvi.points[sample(nrow(ndvi.points), 1000), ]
+#sample.ndvi.points <- ndvi.points[sample(nrow(ndvi.points), 1000), ]
 
 # SERC plots on top of Landat NDVI scores
 ggplot() + 
@@ -83,7 +88,15 @@ ggplot() +
   geom_point(data = species.map[,3:2], aes(x=lat, y=lon), shape=1, color="darkslategray", size =1.5) +
   scale_color_gradient2(midpoint=median(ndvi.points$layer),low="lightblue", mid="bisque", high="darkolivegreen4") +
   labs(x="Latitude", y="Longitude")
-  
-  
 
 
+
+# plotting utm data
+
+ggplot() + 
+  geom_rect(data=species.map, aes(xmin=easting, xmax=easting + 20, ymin=northing, ymax=northing + 20, fill = species), color=NA) + 
+  geom_rect(data=ndvi.points, aes(xmin=easting, xmax=easting + 30, ymin=northing, ymax=northing + 30), color="black", fill = NA)
+
+  #scale_color_gradient2(midpoint=median(ndvi.points$layer),low="lightblue", mid="bisque", high="darkolivegreen4") +
+  #labs(x="Easting", y="Northing")
+  
