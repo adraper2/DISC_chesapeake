8aa5ac4e51e8651e5d9802ac8cc8c4e348026d61
Aidan Draper <adraper2@elon.edu>
2018-07-16 08:57:42 -0400

    push updates

--- run_classifier.R
+++ run_classifier.R
@@ -7,6 +7,21 @@ rm(list=ls())
 library(ggplot2)
 library(randomForest)
 library(gridExtra)
+library(reprtree)
+
+# To install the reprtree package off of GitHub (run once initially then recomment it out)
+#options(repos='http://cran.rstudio.org')
+#have.packages <- installed.packages()
+#cran.packages <- c('devtools','plotrix','randomForest','tree')
+#to.install <- setdiff(cran.packages, have.packages[,1])
+#if(length(to.install)>0) install.packages(to.install)
+#
+#library(devtools)
+#if(!('reprtree' %in% installed.packages())){
+#  install_github('araastat/reprtree')
+#}
+#for(p in c(cran.packages, 'reprtree')) eval(substitute(library(pkg), list(pkg=p)))
+
 
 setwd("~/Documents/Junior_Year/DISC_REU/DISC_chesapeake/")
 load(file='training_set.rda')
@@ -22,6 +37,8 @@ test <- training[-samp,]
 model = randomForest(phau ~ .,data = train[,-c(1:4,5:7,9:12,18)], keep.forest=TRUE)
 
 model
+
+#reprtree:::plot.getTree(model) produce the random forest tree visual
 
 #save(model, file=paste("Classifiers/",curr.species,"_model.rda",sep=""))
 
